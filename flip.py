b0a3c1823cf6084e6a1cd8aad451d22c0bf9bb5a
Chris Dong <cadong@dons.usfca.edu>
2017-07-16 16:45:59 -0700

    finish flip.py

--- flip.py
+++ flip.py
@@ -2,7 +2,20 @@ import sys
 from PIL import Image
 
 # define your flip function here
-...
+def flip(img):
+	width, height = img.size
+
+	imgdup = img.copy()
+
+	m = img.load()
+	m2 = imgdup.load()
+
+	for x in range(width):
+		for y in range(height):
+			m2[x,y] = m[width-x-1,y]
+	return imgdup
+
+
 if len(sys.argv)<=1:
 	print "missing image filename"
 	sys.exit(1)
@@ -12,5 +25,4 @@ img = img.convert("L")
 img.show()
 
 # call your flip function here
-...
-img.show()
+flip(img).show()
