6a344f2734bbc093e5335236a1039e4376caf37e
Chris Dong <cadong@dons.usfca.edu>
2017-07-20 19:48:29 -0700

    finish sharpen.py and complete project

---
+++ sharpen.py
@@ -0,0 +1,29 @@
+from filter import *
+
+def laplace(coord):
+    return sum(coord[1:5]) - 4 * coord[0]
+
+def minus(A, B):
+    width, height = A.size
+
+    imgdup = A.copy()
+
+    pixels = imgdup.load()
+
+    A = A.copy().load()
+    B = B.copy().load()
+
+    for x in range(width):
+        for y in range(height):
+            pixels[x, y] = A[x, y] - B[x,y]
+    return imgdup
+
+img = open(sys.argv)
+img.show()
+
+edges = filter(img, laplace)
+
+minus(img, edges).show()
+
+
+
